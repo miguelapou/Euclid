cd22bfb61420302334ba8e8f6213990f7f7bb33a
shway250 <jackson_duhon@yahoo.com>
2016-10-04 16:05:19 -0700

    made a scale and fixed amplitude

--- js/synth.js
+++ js/synth.js
@@ -1,5 +1,15 @@
 var osc;
 
+///Note Variables
+var A = 220;
+var B = 246.94;
+var C = 277.18;
+var D = 293.66;
+var E = 329.63;
+var F = 369.99;
+var G = 392.00;
+
+
 function Synth(){
 
   this.create = function(){
@@ -9,10 +19,56 @@ function Synth(){
   }
 
   this.control = function(){
-    var freq = map(square.x, 0, width, 40, 880);
+    var freq = map(square.x, 0, width, 0, 150);
+    if(freq >= 0 && freq <10){
+      freq= A;
+    }
+    else if(freq >= 10 && freq <20){
+      freq= B;
+    }
+    else if(freq >= 20 && freq <30){
+      freq= C;
+    }
+    else if(freq >= 30 && freq <40){
+      freq= D;
+    }
+    else if(freq >= 40 && freq <50){
+      freq= E;
+    }
+    else if(freq >= 50 && freq <60){
+      freq= F;
+    }
+    else if(freq >= 60 && freq <70){
+      freq= G;
+    }
+    else if(freq >= 70 && freq <80){
+      freq= A*2;
+    }
+    else if(freq >= 80 && freq <90){
+      freq= B*2;
+    }
+    else if(freq >= 90 && freq <100){
+      freq= C*2;
+    }
+    else if(freq >= 100 && freq <110){
+      freq= D*2;
+    }
+    else if(freq >= 110 && freq <120){
+      freq= E*2;
+    }
+    else if(freq >= 120 && freq <130){
+      freq= F*2;
+    }
+    else if(freq >= 130 && freq <140){
+      freq= G*2;
+    }
+    else if(freq >= 140 && freq <150){
+      freq= A*4;
+    }
+    console.log(freq);
     this.osc.freq(freq);
 
-    var amp = map(mouseY, 0, height, 1, .01);
+    var amp = map(square.y, 0, height, 1, .01);
     this.osc.amp(amp);
   }
 }
