b30b00ecc136f32825b049f67f2bc10d01c664e2
miguelapou <Retrofuture@Miguels-MacBook-Pro.local>
2016-10-03 17:49:16 -0700

    added canvas and hit detection

--- p5BoilerPlate/js/sketch.js
+++ p5BoilerPlate/js/sketch.js
@@ -1,19 +1,106 @@
-// Learning Processing
-// Daniel Shiffman
-// http://www.learningprocessing.com
-
-// Example 1-1: stroke and fill
+var bx;
+var by;
+var boxSize = 75;
+var overBox = false;
+var locked = false;
+var xOffset = 0.0; 
+var yOffset = 0.0; 
 
 function setup() {
-  createCanvas(480, 270);
-  background(0);
+  createCanvas(640, 360);
+  bx = width/2.0;
+  by = height/2.0;
+  rectMode(RADIUS);  
 }
 
 function draw() {
-  if(mouseIsPressed){
-  	fill(0);
-  } else{
-  	fill(255);
+  background(0);
+  
+  // Test if the cursor is over the box 
+  if (mouseX > bx-boxSize && mouseX < bx+boxSize && 
+      mouseY > by-boxSize && mouseY < by+boxSize) {
+    overBox = true;  
+    if(!locked) { 
+      stroke(255); 
+      fill(153);
+    } 
+  } else {
+    stroke(153);
+    fill(153);
+    overBox = false;
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  
+  // Draw the box
+    yEdge();
 }
+
+function yEdge(){
+  if ((locked) && (mouseY >= height - (boxSize/2))) {
+    by = height - (boxSize/2);
+  }else if ((locked) && (mouseY <= 0 + (boxSize/2))){
+    by = 0 + (boxSize/2);
+  }else if ((locked) && (mouseX >= width - (boxSize/2))) {
+    bx = width - (boxSize/2);
+  }else if ((locked) && (mouseX <= 0 + (boxSize/2))){
+    bx = 0 + (boxSize/2);
+  }
+  ellipse(bx, by, boxSize, boxSize);
+}
+
+function mousePressed() {
+  if(overBox) { 
+    locked = true; 
+    fill(255, 255, 255);
+  } else {
+    locked = false;
+  }
+  
+  xOffset = mouseX-bx; 
+  yOffset = mouseY-by; 
+
+}
+
+function mouseDragged() {
+  if(locked) {
+    bx = mouseX-xOffset; 
+    by = mouseY-yOffset; 
+  }
+}
+
+function mouseReleased() {
+  locked = false;
+}
+
+// var osc, fft;
+
+// function setup() {
+//   createCanvas(720, 256);
+
+//   osc = new p5.Oscillator(440, 'sine'); // set frequency and type
+//   osc.amp(.5);
+
+//   fft = new p5.FFT();
+//   osc.start();
+// }
+
+// function draw() {
+//   background(255);
+
+//   var waveform = fft.waveform();  // analyze the waveform
+//   beginShape();
+//   strokeWeight(5);
+//   for (var i = 0; i < waveform.length; i++){
+//     var x = map(i, 0, waveform.length, 0, width);
+//     var y = map(waveform[i], -1, 1, height, 0);
+//     vertex(x, y);
+//   }
+//   endShape();
+
+//   // change oscillator frequency based on mouseX
+//   var freq = map(mouseX, 0, width, 40, 880);
+//   osc.freq(freq);
+//   //console.log(osc.freq);
+
+//   var amp = map(mouseY, 0, height, 1, .01);
+//   osc.amp(amp);
+// }
